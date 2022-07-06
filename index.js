37a41b444740d3d3891df0446d1bc95f2959207b
Sergio Andres <sergiotorchia@hotmail.com>
2022-07-06 14:44:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+logger.info('Mensaje informativo');
+logger.debug('Mensaje debug');
+logger.warn('Mensaje Warning');
+logger.error('Mensaje error');
+
+try {
+  console.log('Funciona normal');
+} catch (e) {
+  console.log('Hubo un error');
+}
